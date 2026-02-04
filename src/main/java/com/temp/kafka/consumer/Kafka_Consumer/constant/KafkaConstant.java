b866402fcc87f7050cebe15f42590303be3f401f
aruns <devila.arun@gmail.com>
2026-02-04 18:35:18 +0530

    Commit

---
+++ src/main/java/com/temp/kafka/consumer/Kafka_Consumer/constant/KafkaConstant.java
@@ -0,0 +1,7 @@
+package com.temp.kafka.consumer.Kafka_Consumer.constant;
+
+public class KafkaConstant {
+    public static final String NEW_TOPIC = "NEW_TOPIC";
+    public static final String PRO_RETRY_TOPIC = "PRO_RETRY_TOPIC";
+    public static final String DLT_TOPIC = "DLT_TOPIC";
+}
---
+++ src/main/java/com/temp/kafka/consumer/Kafka_Consumer/constant/ProductStatus.java
@@ -0,0 +1,6 @@
+package com.temp.kafka.consumer.Kafka_Consumer.constant;
+
+public enum ProductStatus {
+    AVAILABLE,
+    NOT_AVAILABLE
+}
